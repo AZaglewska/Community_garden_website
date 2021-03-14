12a0118848bb21a290f27c90196132a7ee207e56
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-14 22:39:25 +0100

    adding routes

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+export const routes = {
+  home: "/",
+  articles: "/aktualności",
+  fullArticle: "/artykuł/:title",
+  gallery: "/galeria",
+  gallery1: "/galeria1",
+  gallery2: "/galeria2",
+  regulations: "/regulamin",
+  contact: "/kontakt",
+};
