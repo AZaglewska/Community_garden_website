ef67bb8975bf252b38a1781c2e938ec7281d6da6
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-16 19:21:34 +0100

    adding getHomeArticleData function to get a data from contentful client, adding homeArticles state

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,11 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Router from "../routing/Router";
 import CommunityGardenContext from "../context/context";
+import client from "../client/Client";
 
 const Root = () => {
+  const [homeArticles, setHomeArticles] = useState([]);
+
+  const getHomeArticlesData = (dataArticles) => {
+    let articlesData = dataArticles.map((dataArticle) => {
+      const { id } = dataArticle.sys;
+
+      const articleTitle = dataArticle.fields.articleTitle;
+      const articleImage = dataArticle.fields.articleImage.fields.file.url;
+      const articleDate = dataArticle.fields.articlesDate;
+      const articleContent = [...dataArticle.fields.articles.content];
+
+      const article = {
+        id,
+        articleTitle,
+        articleImage,
+        articleDate,
+        articleContent,
+      };
+      return article;
+    });
+
+    setHomeArticles(articlesData);
+  };
+
+  useEffect(() => {
+    client
+      .getEntries({
+        content_type: "rodArticles",
+        limit: 2,
+      })
+
+      .then((response) => {
+        getHomeArticlesData(response.items);
+      });
+  }, []);
+
   return (
     <>
-      <CommunityGardenContext.Provider value={{}}>
+      <CommunityGardenContext.Provider value={{ homeArticles }}>
         <Router />
       </CommunityGardenContext.Provider>
     </>
