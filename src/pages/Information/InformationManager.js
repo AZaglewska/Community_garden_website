9478207ef787a5bd92345d69cdfb4fc99a56cb2b
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-20 23:20:52 +0100

    adding InformationManager.js, SummerHouse.js and styles

---
+++ src/pages/Information/InformationManager.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./InformationManager.scss";
+
+const InformationManager = () => {
+  return (
+    <>
+      <h1>Organizacja działki</h1>
+    </>
+  );
+};
+
+export default InformationManager;
---
+++ src/pages/Information/InformationPages/SummerHouse.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const SummerHouse = () => {
+  return (
+    <>
+      <div>
+        <h2>ALTANA</h2>
+        <h3>REGULAMIN ROD:</h3>
+        <ul>
+          <ol>
+            <h4>§ 44</h4>
+            <li>
+              1. Altana działkowa stanowi wolno stojący budynek
+              rekreacyjno-wypoczynkowy lub inny obiekt budowlany spełniający
+              taką funkcję, położony na terenie działki w ROD. Altana działkowa
+              powinna być funkcjonalna i estetyczna.
+            </li>
+            <li>
+              2. Altana działkowa może mieć powierzchnię zabudowy mierzoną po
+              obrysie ścian zewnętrznych do 35 m2. Do powierzchni zabudowy nie
+              wlicza się tarasu, werandy lub ganku, o ile ich łączna
+              powierzchnia nie przekracza 12 m2.
+            </li>
+            <li>
+              3. Altana działkowa może mieć wysokość do 5 metrów przy dachu
+              stromym i 4 metrów przy dachu płaskim.
+            </li>
+            <li>
+              4. Wysokość altany działkowej mierzy się od poziomu gruntu do
+              najwyższego punktu dachu.
+            </li>
+            <li>
+              5. Odległość altany działkowej od granic działki nie może być
+              mniejsza niż 3 metry.
+            </li>
+            <li>
+              6. Jeżeli plan zagospodarowania ROD określa miejsce usytuowania
+              altany działkowej, działkowiec może wybudować altanę działkową
+              tylko zgodnie z tym planem.
+            </li>
+          </ol>
+        </ul>
+        <ul>
+          <h4>§ 45</h4>
+          <ol>
+            Działkowiec obowiązany jest powiadomić na piśmie zarząd ROD o
+            zamiarze budowy, nadbudowy lub rozbudowy altany działkowej,
+            załączając rysunek uwzględniający jej powierzchnię zabudowy i
+            wysokość oraz usytuowanie względem granic działki zgodnie z
+            parametrami określonymi w § 44.
+          </ol>
+        </ul>
+        <ul>
+          <h4>§ 46</h4>
+          <ol>
+            1. Zarząd ROD nakazuje działkowcowi wstrzymanie budowy, nadbudowy
+            lub rozbudowy altany działkowej w przypadku zaistnienia przesłanek
+            uzasadniających podejrzenie naruszenia przepisów regulaminu.
+          </ol>
+          <ol>
+            2. W przypadku stwierdzenia przez zarząd ROD budowy, nadbudowy lub
+            rozbudowy altany działkowej z naruszeniem przepisów regulaminu,
+            działkowiec zobowiązany jest do usunięcia nieprawidłowości, a nawet
+            do rozebrania budowanej lub rozbudowywanej altany działkowej, jeśli
+            naruszenia nie mogą zostać usunięte w inny sposób.
+          </ol>
+          <ol>
+            3. W przypadku powzięcia informacji, że na terenie działki
+            wybudowano, nadbudowano lub rozbudowano altanę działkową lub inny
+            obiekt z naruszeniem przepisów prawa, zarząd ROD, w imieniu PZD,
+            zgłasza naruszenie do właściwego organu administracji publicznej.
+          </ol>
+          <ol>
+            4. Stwierdzenie przez właściwy organ administracji publicznej
+            naruszenia prawa, o którym mowa w ust. 3, stanowi podstawę do
+            zastosowania sankcji określonych w ustawie.
+          </ol>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default SummerHouse;
