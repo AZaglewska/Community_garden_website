72af86837a9c5e3d7a7afb1b4a82439b0ac0f4b9
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-01 22:41:15 +0200

    modifying Statute.js, adding statuteName from contentful, adding prices

--- src/pages/InformationPages/StatutePages/Statute.js
+++ src/pages/InformationPages/StatutePages/Statute.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import CommunityGardenContext from "../../../context/context";
 import "./Statute.scss";
 
-const Regulations = () => {
+const Statute = () => {
   const context = useContext(CommunityGardenContext);
 
   const { statuteData } = context;
@@ -10,18 +10,25 @@ const Regulations = () => {
   return (
     <>
       <div className="statute">
-        <h1 className="statute__title">Regulamin</h1>
+        <h1 className="statute__title">Regulamin i Opłaty</h1>
         <div className="statute__wrapper">
           <ul className="statute__list">
             {statuteData.map((statuteDataElement) => {
-              const { id, statuteIcon, statuteFile } = statuteDataElement;
+              const {
+                id,
+                statuteIcon,
+                statuteFile,
+                statuteName,
+              } = statuteDataElement;
               return (
                 <li key={id} className="statute__element">
                   <img src={statuteIcon} className="statute__icon" />
+                  <h3 className="statute__subtitle">{statuteName}</h3>
                   <a
                     href={statuteFile}
                     target="_blank"
                     className="statute__file"
+                    rel="noreferrer"
                   >
                     Regulamin.pdf
                   </a>
@@ -29,10 +36,44 @@ const Regulations = () => {
               );
             })}
           </ul>
+          <div className="prices">
+            <h4 className="prices__title">Opłaty</h4>
+            <ul className="prices__list">
+              <li>
+                <h4 className="prices__subtitle">
+                  Wpłaty można dokonać na konto ogrodu w Banku Zachodnim WBK Nr:
+                </h4>
+                <p className="prices__text">63 1090 1870 0000 1429 7475</p>
+                <h4 className="prices__subtitle">W tytule należy podać :</h4>
+              </li>
+
+              <li className="prices__elements">
+                opłata ogrodowa za działkę nr [...]
+              </li>
+              <li className="prices__elements">
+                (składka członkowska, opłata za prąd według stanu licznika)
+              </li>
+            </ul>
+            <ul className="prices__list">
+              <h4 className="prices__subtitle">
+                Obliczenie rocznej opłaty za działkę:
+              </h4>
+              <li>
+                <span>Opłata ogrodowa:</span> 0,90pln za m<sup>2</sup>
+              </li>
+              <li>
+                <span>Składka członkowska:</span> 6,0 pln
+              </li>
+              <li>
+                <span>Energia elektryczna:</span> 0,60 pln x zużycie w kWh*
+              </li>
+              <li>*zgodnie ze wskazaniem licznika</li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default Regulations;
+export default Statute;
