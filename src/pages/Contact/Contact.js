099bf2c5ef90f9812710698c7f3eb650bafd7992
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-21 11:31:46 +0200

    modifying Contact structures, adding links and routes, adding ContactForm.js and ContactInfo.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,23 @@
 import React from "react";
+import ContactForm from "./ContactForm/ContactForm";
+import ContactInfo from "./ContactInfo/ContactInfo";
+import { Link } from "react-router-dom";
+import { routes } from "../../routes";
 import "./Contact.scss";
 
 const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <div className="contact">
+        <h1 className="contact__title">Kontakt</h1>
+        <Link to={routes.home} className="contact__link">
+          Powrót do strony głównej
+        </Link>
+        <div className="contact__components">
+          <ContactForm />
+          <ContactInfo />
+        </div>
+      </div>
     </>
   );
 };
