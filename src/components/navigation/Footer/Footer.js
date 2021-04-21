0806e36bad8523d7fbd38dbce9e8d3847e28ae8d
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-21 10:58:06 +0200

    adding routes, improving Footer.js structures, adding images

--- src/components/navigation/Footer/Footer.js
+++ src/components/navigation/Footer/Footer.js
@@ -1,38 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Footer.scss";
+import { routes } from "../../../routes/";
 import LogoAnn from "../../../assets/Logo/AnnLogo.svg";
 import LogoFooter from "../../../assets/Logo/LogoFooter.svg";
 import LogoPzd from "../../../assets/Logo/logo-pzd.png";
+import imageDandelion from "../../../assets/Images/FooterImages/dandelion-seeds-2483277_1920.png";
+import imageFlower from "../../../assets/Images/FooterImages/rose-732361_1280.png";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
     <>
       <div className="footer">
-        <div>
-          <Link to="/">
-            <img src={LogoFooter} className="footer__main-logo" />
-          </Link>
-        </div>
-        <div className="footer__container">
-          <div className="footer__elements">
-            <h3 className="footer__title">
-              Rodzinny Ogród Działkowy <span>Warszawianka</span>
-            </h3>
-            <p className="footer__address">
-              ul.Piaseczyńska 60,<span>00-765 Warszawa</span>
-            </p>
+        <div className="footer__wrapper">
+          <img
+            className="footer__image image-rotate"
+            src={imageDandelion}
+            alt="dandelion"
+          />
+          <div>
+            <Link to={routes.home}>
+              <img
+                src={LogoFooter}
+                className="footer__main-logo"
+                alt="main-logo"
+              />
+            </Link>
+            <div className="footer__elements">
+              <Link to={routes.contact}>
+                <button className="footer__button">Napisz do nas</button>
+              </Link>
+              <p className="footer__address">
+                ul.Piaseczyńska 60,<span>00-765 Warszawa</span>
+              </p>
+              <a href="http://pzd.pl/" target="_blank" rel="noreferrer">
+                <img
+                  src={LogoPzd}
+                  className="footer__logo-pzd"
+                  alt="pzd-logo"
+                />
+              </a>
+            </div>
           </div>
-          <div className="footer__details">
-            Created by
-            <a href="https://github.com/AZaglewska" target="_blank">
-              <img src={LogoAnn} className="footer__logo" alt="logo" />
-            </a>
-          </div>
+          <img className="footer__image " src={imageFlower} alt="flower" />
         </div>
-        <div>
-          <a href="http://pzd.pl/" target="_blank">
-            <img src={LogoPzd} className="footer__logo-pzd" />
+        <div className="footer__details">
+          <p>Created by</p>
+          <a
+            href="https://github.com/AZaglewska"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={LogoAnn}
+              className="footer__logo"
+              alt="ann-logo"
+              rel="noreferrer"
+            />
           </a>
         </div>
       </div>
