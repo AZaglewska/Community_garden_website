4cc8b4b1c0ac55c6d75c76f8924047e5ecb95d01
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-23 21:55:08 +0200

    adding background container

--- src/pages/FullArticle/FullArticle.js
+++ src/pages/FullArticle/FullArticle.js
@@ -9,26 +9,22 @@ const FullArticle = (props) => {
 
   return (
     <>
-      <div>
-        <div className="full-article">
+      <div className="article-background" />
+      <div className="full-article">
+        <div className="full-article__elements">
           <h1 className="full-article__title">Aktualności</h1>
           <Link to={routes.articles} className="full-article__link">
             Powrót do artykułów
           </Link>
-          <div className="full-article__wraper">
-            <img
-              src={image}
-              alt="articleImage"
-              className="full-article__image"
-            />
-            <h3 className="full-article__subtitle">{title}</h3>
+        </div>
 
-            <Markdown source={text} className="full-article__content" />
-
-            <h4 className="full-article__date">
-              Dodano: {`${date.slice(0, 10)}`}
-            </h4>
-          </div>
+        <div className="full-article__wraper">
+          <img src={image} alt="articleImage" className="full-article__image" />
+          <h3 className="full-article__subtitle">{title}</h3>
+          <Markdown source={text} className="full-article__content" />
+          <h4 className="full-article__date">
+            Dodano: {`${date.slice(0, 10)}`}
+          </h4>
         </div>
       </div>
     </>
