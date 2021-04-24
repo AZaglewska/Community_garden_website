bfe306d5407ce926abfbe1a1d3f786b81e353c32
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-24 16:01:13 +0200

    adding showArrowTop feature and isHamburgerMenuOpen function

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/navigation/Navbar/Navbar";
 import Footer from "../components/navigation/Footer/Footer";
+import CommunityGardenContext from "../context/context";
+import { animateScroll as scroll } from "react-scroll";
+import "./MainTemplate.scss";
+import arrowUpIcon from "../assets/Icons/up-arrow-site.svg";
 
 const MainTemplate = ({ children }) => {
+  const context = useContext(CommunityGardenContext);
+  const { showArrowTop, isHamburgerMenuOpen } = context;
   return (
     <>
       <Navbar />
+      {showArrowTop && isHamburgerMenuOpen === false ? (
+        <img
+          src={arrowUpIcon}
+          className="arrowUp"
+          onClick={() => scroll.scrollToTop()}
+          alt="arrow"
+        />
+      ) : null}
       {children}
       <Footer />
     </>
