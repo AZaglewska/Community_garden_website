b93a33b6d63a7310b1408b31d5c83e39f4d68bbc
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-05-27 17:27:58 +0200

    improving Statute.js structures

--- src/pages/InformationPages/StatutePages/Statute.js
+++ src/pages/InformationPages/StatutePages/Statute.js
@@ -22,12 +22,8 @@ const Statute = () => {
         <div className="statute__wrapper">
           <ul className="statute__list">
             {statuteData.map((statuteDataElement) => {
-              const {
-                id,
-                statuteIcon,
-                statuteFile,
-                statuteName,
-              } = statuteDataElement;
+              const { id, statuteIcon, statuteFile, statuteName } =
+                statuteDataElement;
               return (
                 <li key={id} className="statute__element">
                   <img
@@ -49,40 +45,45 @@ const Statute = () => {
             })}
           </ul>
           <div className="prices">
-            <h4 className="prices__title">Opłaty</h4>
-            <ul className="prices__list">
+            <h4 className="prices__title">Opłaty na 2021 rok</h4>
+            <div className="prices__list">
+              <p>
+                Zgodnie z postanowieniami{" "}
+                <span>Walnego Zebrania Zarządu ROD</span>w roku 2021 zostały
+                uchwalone następujące opłaty ogrodowe:
+              </p>
+              <ul className="prices__list-elements">
+                <li className="prices__elements">
+                  <span>Opłata ogrodowa:</span> 0,90 zł/m<sup>2</sup>
+                </li>
+                <li className="prices__elements">
+                  <span>Składka członkowska:</span> 6,00 zł
+                </li>
+                <li className="prices__elements">
+                  <span>Energia elektryczna:</span> 0,60 zł/kWh*
+                </li>
+                <div className="prices__elements prices__smaller-text">
+                  *zgodnie ze wskazaniem licznika
+                </div>
+                <li className="prices__elements">
+                  <span>Opłata energetyczna:</span> 15,00 zł/działka
+                </li>
+                <li className="prices__elements">
+                  <span>Wywóz śmieci:</span> 100,00 zł/działka
+                </li>
+              </ul>
               <li>
                 <h4 className="prices__subtitle">
                   Wpłaty można dokonać na konto ogrodu w Banku Zachodnim WBK Nr:
                 </h4>
-                <p className="prices__text">63 1090 1870 0000 1429 7475</p>
+                <p className="prices__text">63 1090 1870 0000 0001 1429 7475</p>
                 <h4 className="prices__subtitle">W tytule należy podać :</h4>
               </li>
-
-              <li className="prices__elements">
-                opłata ogrodowa za działkę nr [...]
-              </li>
-              <li className="prices__elements">
-                (składka członkowska, opłata za prąd według stanu licznika)
-              </li>
-            </ul>
-            <ul className="prices__list">
-              <h4 className="prices__subtitle">
-                Obliczenie rocznej opłaty za działkę:
-              </h4>
-              <li className="prices__elements">
-                <span>Opłata ogrodowa:</span> 0,90pln za m<sup>2</sup>
-              </li>
-              <li className="prices__elements">
-                <span>Składka członkowska:</span> 6,0 pln
-              </li>
-              <li className="prices__elements">
-                <span>Energia elektryczna:</span> 0,60 pln x (zużycie w kWh)*
-              </li>
-              <li className="prices__elements prices__smaller-text">
-                *zgodnie ze wskazaniem licznika
-              </li>
-            </ul>
+              <p className="prices__elements">
+                opłata ogrodowa za działkę nr [...] (składka członkowska, opłata
+                za prąd według stanu licznika)
+              </p>
+            </div>
           </div>
         </div>
       </div>
